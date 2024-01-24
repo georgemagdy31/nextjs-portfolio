0830f9ee6d59cced6713173e6fec304f1707fdf0
georgemagdy31 <georgemagdy718@gmail.com>
2024-01-24 20:07:14 +0200

    finall

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    
     <html lang="en">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"></meta>
       <body className={inter.className}>{children}</body>
     </html>
   );
---
+++ components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer  z-10  text-white">
+      <div className="container p-12 flex justify-center mb-12">
+        
+        <p className="text-slate-600">All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
